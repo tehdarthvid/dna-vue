fed267b50b9a179c5dfb2913439a77928d6ec0a4
darthvid <darth@zergling.local>
2018-04-14 17:04:05 +0800

    ESLint fixes

--- src/views/Deck.vue
+++ src/views/Deck.vue
@@ -18,7 +18,7 @@ export default {
   name: 'Deck',
   components: { Card, ImageModal },
   props: {
-    cardList: { type: Array, default: [] }
+    cardList: { type: Array, default: () => [] }
   }
 };
 </script>
