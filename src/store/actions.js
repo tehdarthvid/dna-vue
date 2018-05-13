d8f8d7964a73df379cdbde2f172d1c10336cdc14
darthvid <darth@zergling.local>
2018-05-13 14:38:17 +0800

    Add new activeDeck state in store. Use axios for API call.

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,27 @@
-import { dlog } from '@/utils';
+import axios from 'axios';
+import { dlog, error } from '@/utils';
 
 export default {
   setActiveCard({ commit }, card) {
     dlog(`action setActiveCard ${card.title}`);
     commit('setActiveCard', card);
+  },
+  setActiveDeck({ commit }, cards) {
+    dlog(`action setActiveDeck ${cards.length()}`);
+    commit('setActiveDeck', cards);
+  },
+  initStore({ commit }) {
+    dlog(`action initStore`);
+    axios
+      .get(
+        `https://us-central1-playground-dna.cloudfunctions.net/api/graphql/?query={cards%20{title%20bgImageURL%20date}}`
+      )
+      .then(response => {
+        dlog(`initStore promise then`);
+        commit(`setActiveDeck`, response.data.data.cards);
+      })
+      .catch(e => {
+        error(`initStore promise catch ${e}`);
+      });
   }
 };
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,5 +4,9 @@ export default {
   setActiveCard(state, card) {
     dlog(`mutation setActiveCard ${card.title}`);
     state.activeCard = card;
+  },
+  setActiveDeck(state, cards) {
+    dlog(`mutation setActiveDeck ${cards.length}`);
+    state.activeDeck = cards;
   }
 };
